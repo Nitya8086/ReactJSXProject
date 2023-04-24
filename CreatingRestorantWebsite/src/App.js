3e86c85f157b5139cfd83aff1fedb54ee934a9db
Nitya Patel <nityapatel450@gmail.com>
2023-04-24 15:19:39 +0530

    addingModalAndPortal

--- CreatingRestorantWebsite/src/App.js
+++ CreatingRestorantWebsite/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import {Fragment} from "react";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
