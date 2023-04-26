253cb9a23145a2405a703292c08dd06b0e4281d7
Nitya Patel <nityapatel450@gmail.com>
2023-04-26 12:44:32 +0530

    Showing and Enabling the delete of Cart Items

--- CreatingRestorantWebsite/src/components/Cart/Cart.js
+++ CreatingRestorantWebsite/src/components/Cart/Cart.js
@@ -1,16 +1,38 @@
+import {useContext } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import CartContext from '../../Store/CartContext';
 
 const Cart = props =>{
 
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+    const hasItems = cartCtx.items.length>0;
+
+    const cartItemRemoveHandeler = id =>{
+        cartCtx.removeItem(id);
+
+    };
+    const cartItemAddHandeler = item=>{
+        cartCtx.addItem(item);
+        
+    };
+
     const cartItems  = 
     (<ul className={classes['cart-items']}>
-        {[{
-        id : 'c1',
-        name : 'Sushi',
-        amount : 2,
-        price : 12.99, }].map((item) =>(
-        <li>{item.name}</li>
+        {cartCtx.items.map((item) =>(
+       
+        <CartItem 
+        key = {item.id} 
+        name = {item.name} 
+        amount = {item.amount} 
+        price = {item.price} 
+        onRemove = {cartItemRemoveHandeler.bind(null,item.id)} 
+        onAdd = {cartItemAddHandeler.bind(null,item)}
+        />
         ))}
     </ul>
 );
@@ -20,12 +42,12 @@ return (
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={classes.actions}>
             <span></span>
             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            <button className={classes.button}>Order</button>
+          { hasItems &&  <button className={classes.button}>Order</button>}
 
         </div>
     </Modal>
