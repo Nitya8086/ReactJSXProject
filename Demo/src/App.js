5ec4b6faf7d10429b9f0c14d7e68df7e9e01e2c9
Nitya Patel <nityapatel450@gmail.com>
2023-07-07 15:55:45 +0530

    adding new movie into list

--- Demo/src/App.js
+++ Demo/src/App.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect,useCallback} from 'react';
-
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -12,22 +12,23 @@ function App() {
   setIsLoading(true);
   setError(null);
   try{
-    const response = await fetch('https://swapi.dev/api/films/')
+    const response = await fetch('https://react-http-88c6a-default-rtdb.firebaseio.com/movie.json')
      if(!response.ok){
     throw new Error('Something went wrong');
   }
- 
   const data = await response.json();
+  const loadedMovies = [];
+
+   for(const key in data){
+    loadedMovies.push({
+      id:key,
+      title :data[key].title,
+      openingText : data[key].openingText,
+      releaseDate : data[key].releaseDate
+    })
+   }
  
-    const transformedMovies = data.results.map((movieData) =>{
-      return{
-        id:movieData.episode_id,
-        title : movieData.title,
-        openingText : movieData.opening_crawl,
-        releaseDate : movieData.release_date
-      }
-    });
-    setMovies(transformedMovies);
+   setMovies(loadedMovies);
    
 
   }
@@ -41,6 +42,19 @@ function App() {
   useEffect(() =>{
     fetchMoviesHnadeler();
   },[fetchMoviesHnadeler]);
+
+   const addMovieHandeler = async (movie) =>{
+   const response =  await fetch('https://react-http-88c6a-default-rtdb.firebaseio.com/movie.json',{
+    method:'POST',
+    body : JSON.stringify(movie),
+    header:{
+      'Content-type' :'application/json'
+    }
+   });
+  
+const data = await response.json();
+console.log(data);
+  }
 
   let content = <p>Found No Movie</p>;
    
@@ -58,6 +72,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie = {addMovieHandeler}/>
+      </section>
       <section>
         <button onClick={fetchMoviesHnadeler}>Fetch Movies</button>
       </section>
